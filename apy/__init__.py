d636ed3c0dc70a4b6a2f0c7473a4019ec811ea72
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2020-06-20 18:09:06 +0200

    bump: v0.5.3 --> v0.6.0

--- apy/__init__.py
+++ apy/__init__.py
@@ -1,5 +1,5 @@
 """Package for interfacing and manipulating Anki decks"""
-__version__ = '0.5.3'
+__version__ = '0.6.0'
 
 try:
     import anki
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
     name='apy',
-    version='0.5.3',
+    version='0.6.0',
     description='CLI script for interacting with local Anki collection',
     long_description=readme(),
     url='https://github.com/lervag/apy',
